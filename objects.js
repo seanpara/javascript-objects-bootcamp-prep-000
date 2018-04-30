11767860378cdc93fe98e960b65a635b657bc74b
Sean Logan Para <spara77@gmail.com>
2018-04-30 22:57:07 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -2,4 +2,4 @@ var playlist = {
   radiohead: "lotus flower", theStrokes: "Is This It?", theTalkingHeads: "The Great Curve", lcdSoundystem: "Home", animalCollective: "The Purple Bottle", theBeachBoys: "Sloop John B" , theSmiths: "Some Girls Are Bigger Than Others", 
 }
 
-function updatePlau
+function updatePlaylist(playlist, artistName, songTitle)
