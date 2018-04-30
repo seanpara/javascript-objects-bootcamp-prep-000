092d801d757b8d764bc876ad0e0b791e3f96f846
Sean Logan Para <spara77@gmail.com>
2018-04-30 22:56:52 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,3 +1,5 @@
 var playlist = {
   radiohead: "lotus flower", theStrokes: "Is This It?", theTalkingHeads: "The Great Curve", lcdSoundystem: "Home", animalCollective: "The Purple Bottle", theBeachBoys: "Sloop John B" , theSmiths: "Some Girls Are Bigger Than Others", 
 }
+
+function updatePlau
